122aff00c208021bbc16f18163efb02f692aa196
zhenfa <oveu123@gmail.com>
2022-12-08 20:46:16 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', async (ctx, next) => {
 })
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+  ctx.body = 'koa2 string test'
 })
 
 router.get('/json', async (ctx, next) => {
